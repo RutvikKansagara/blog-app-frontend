eba91eee5a8d44a69890cb736e2bd974ca06e56d
RutvikKansagara <rutvikkansagara7852@gmail.com>
2023-11-06 18:33:12 +0530

    frontend

--- src/pages/blog/YourBlogs.jsx
+++ src/pages/blog/YourBlogs.jsx
@@ -32,7 +32,7 @@ const YourBlogs = () => {
   return (
     <>
       {loading && <Loader text="Loading ..." />}
-      {!loading && userBlogs && (
+      {!loading && userAllBlogs && (
         <section className="bg-white dark:bg-gray-900">
           <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
             <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
